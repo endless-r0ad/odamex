00acb20c098253c1c99e0bbdb5e4029a45e1cb57
endless-r0ad <24723883+endless-r0ad@users.noreply.github.com>
2026-04-16 16:42:51 -0400

    simplify atSnapshotInterval

--- client/src/cl_demo.cpp
+++ client/src/cl_demo.cpp
@@ -70,7 +70,7 @@ int LatestDemoVersion(const int version)
 
 NetDemo::NetDemo()
     : state(st_stopped), oldstate(st_stopped), filename(""), demofp(NULL), netdemotic(0),
-      pause_netdemotic(0)
+      pause_netdemotic(0), last_map_tic(0)
 {
 	memset(&header, 0, sizeof(header));
 }
@@ -146,7 +146,7 @@ void NetDemo::cleanUp()
 	snapshot_index.clear();
 	map_index.clear();
 	state = oldstate = NetDemo::st_stopped;
-	netdemotic = pause_netdemotic = 0;
+	netdemotic = pause_netdemotic = last_map_tic = 0;
 }
 
 /**
@@ -605,10 +605,9 @@ void NetDemo::writeChunk(const byte *data, size_t size, netdemo_message_t type)
 //
 bool NetDemo::atSnapshotInterval()
 {
-	if (!connected || map_index.empty() || gamestate != GS_LEVEL)
+	if (!connected || gamestate != GS_LEVEL)
 		return false;
 
-	int last_map_tic = map_index.back().ticnum;
 	if (gametic == last_map_tic)
 		return false;
 
@@ -1234,6 +1233,7 @@ void NetDemo::writeMapChange()
 	{
 		writeSnapshotData(snapbuf);
 		writeChunk(snapbuf.data(), snapbuf.size(), NetDemo::msg_map_change);
+		last_map_tic = gametic;
 	}
 }
 
